959d67b1be143f956715e91dcfbd26bf77695a7c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-23 22:17:54 +0530

    build-ak3: Specify whether thin LTO in zip name
    Change-Id: Iad5a75142440f4f662abc7528459bbd8a72cb378

--- build-ak3.sh
+++ build-ak3.sh
@@ -5,10 +5,10 @@
 
 SECONDS=0 # builtin bash timer
 
-ZIPNAME="aospa-gki-marble-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="aospa-gki-marble-$(date '+%Y%m%d-%H%M')"
 if test -z "$(git -C common rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git -C common rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+	ZIPNAME="${ZIPNAME}-$(echo $head | cut -c1-8)"
 fi
 
 echo -e "Starting build...\n"
@@ -29,12 +29,16 @@ while [ "$#" -gt 0 ]; do
 	shift
 done
 
+if [[ $LTO != "full" ]]; then
+	ZIPNAME="${ZIPNAME}-thin"
+fi
+
 build/build.sh 2> >(tee error.log >&2) || exit $?
 
 echo -e "\nKernel built succesfully! Zipping up...\n"
 cp out/android12-5.10/dist/Image AnyKernel3
 cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+zip -r9 "../$ZIPNAME.zip" * -x .git README.md *placeholder
 cd ..
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) :"
-echo "$(realpath $ZIPNAME)"
+echo "$(realpath $ZIPNAME.zip)"
